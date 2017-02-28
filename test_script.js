ed77d098fdb2362098374ac482b2efbc13d04fbd
c-ywj <cybc00@hotmail.com>
2017-02-28 21:00:24 +0000

    clean up

--- test_script.js
+++ test_script.js
@@ -35,16 +35,5 @@ client.connect((err) => {
     return console.error("Connection Error", err);
   }
 
-  // client.query(`SELECT * FROM famous_people  WHERE first_name = '${name}' OR last_name = '${name}'`, (err, result) => {
-  //   console.log('Searching ...');
-  //   if (err) {
-  //     return console.error("error running query", err);
-  //   }
-
-  //   var data = result.rows;
-  //   data.forEach((person) => {
-  //     console.log(`Found ${data.length} Person(s) by the name '${name}': - ${person.id}: ${person.first_name} ${person.last_name}, born '${(person.birthdate).toLocaleString().slice(0, 9)}'`); //output: 1
-  //   })
-
   do_q(client, name);
 });
